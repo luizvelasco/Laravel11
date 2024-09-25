c78c83aa990162e397d85021b8842c7dae6bc6d6
luizvelasco <luizvelasco@gmail.com>
2024-09-25 19:01:47 -0300

    Criar cadastrar registro no banco de dados com seeders

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Celke</title>
+</head>
+<body>
+    <h1>Ven-vindo ao Laravel 11</h1>
+
+    <a href="{{ route('courses.index') }}">Listar</a>
+</body>
+</html>
