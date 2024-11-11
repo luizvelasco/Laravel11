6c66e991f24d0df903091e4e2f30b19c42cad4d1
luizvelasco <luizvelasco@gmail.com>
2024-11-11 10:07:28 -0300

    visualizar aula

--- resources/views/classes/show.blade.php
+++ resources/views/classes/show.blade.php
@@ -1,22 +1,60 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h2>Detalhes da aula</h2>
 
-    <a href="{{ route('classe.index', ['course' => $classe->course_id]) }}" class="link-button">Aulas</a><br>
-    <a href="{{ route('classe.edit', ['classe' => $classe->id])}}" class="link-button">Editar</a><br>
+<div class="container-fluid px-4">
+    <div class="mb-1 hstack gap-2">
+        <h2 class="mt-3">Aula</h2>
 
-    <x-alert />
+        <ol class="breadcrumb mb-3 mt-3 ms-auto">
+            <li class="breadcrumb-item">
+                <a href="#" class="text-decoration-none">Dashboard</a>
+            </li>
+            <li class="breadcrumb-item active">Aula</li>
+        </ol>
+    </div>
 
-    <div class="course-list">
-        <div class="course-item">
-            <p>Nome: {{ $classe->name }} </p>
-                <p>Ordem: {{ $classe->order_classe }} </p>
-                <p>Descrição: {{ $classe->description }} </p>
-                <p>Curso: {{ $classe->course->name }}</p> 
-                <p>Criado em: {{ \Carbon\Carbon::parse($classe->created_at)->format('d/m/Y H:i:s') }}</p>
-                <p>Atualizado em: {{ \Carbon\Carbon::parse($classe->updated_at)->format('d/m/Y H:i:s') }}</p>
+    <div class="card mb-4">
+        <div class="card-header hstack gap-2">
+            <span>Visualizar</span>
+            <span class="ms-auto d-sm-flex flex-row">
+                <a href="{{ route('classe.index', ['course' => $classe->course_id]) }}" class="btn btn-info btn-sm me-1 mb-1 mb-sm-0">Aulas</a>
+                <a href="{{ route('classe.edit', ['classe' => $classe->id])}}" class="btn btn-warning btn-sm me-1 mb-1 mb-sm-0">Editar</a>
+
+                <form action="{{ route('classe.destroy', ['classe' => $classe->id]) }}" method="POST">
+                    @csrf
+                    @method('delete')
+                    <button type="submit"  class="btn btn-danger btn-sm me-1 mb-1 mb-sm-0" onclick="return confirm('Tem certeza que deseja apagar este registro?')">Apagar</button>
+                </form>
+            </span>
+        </div>
+        <div class="card-body">
+            <x-alert />
+
+            <dl class="row">
+                <dt class="col-sm-3">Nome: </dt>
+                <dd class="col-sm-9">{{ $classe->name }}</dd>
+
+                <dt class="col-sm-3">Ordem: </dt>
+                <dd class="col-sm-9">{{ $classe->order_classe }}</dd>
+
+                <dt class="col-sm-3">Descrição: </dt>
+                <dd class="col-sm-9">{{ $classe->description }}</dd>
+
+                <dt class="col-sm-3">Curso: </dt>
+                <dd class="col-sm-9">{{ $classe->course->name }}</dd>
+
+                <dt class="col-sm-3">Cadastrado</dt>
+                <dd class="col-sm-9">{{ \Carbon\Carbon::parse($classe->created_at)->format('d/m/Y H:i:s') }}</dd>
+
+                <dt class="col-sm-3">Editado</dt>
+                <dd class="col-sm-9">{{ \Carbon\Carbon::parse($classe->upldated_at)->format('d/m/Y H:i:s') }}</dd>
+            </dl>
+            
         </div>
     </div>
-    
+
+
+</div>
+
 @endsection
