47977b510f3bd1f0ed06b8d3fdd28481e8d84ac9
luizvelasco <luizvelasco@gmail.com>
2024-11-15 11:36:50 -0300

    Layout editar e cadastrar classe

--- resources/views/classes/create.blade.php
+++ resources/views/classes/create.blade.php
@@ -2,30 +2,56 @@
 
 @section('content')
 
-    <h2>Cadastrar Aula</h2>
+<div class="container-fluid px-4">
+    <div class="mb-1 hstack gap-2">
+        <h2 class="mt-3">Aula</h2>
 
-    <a href="{{ route('classe.index', ['course' => $course->id]) }}" class="link-button">Aulas</a><br>
+        <ol class="breadcrumb mb-3 mt-3 ms-auto">
+            <li class="breadcrumb-item">
+                <a href="#" class="text-decoration-none">Dashboard</a>
+            </li>
+            <li class="breadcrumb-item">
+                <a href="{{ route('classe.index', ['course' => $course->id]) }}" class="text-decoration-none">Aulas</a>
+            </li>
+            <li class="breadcrumb-item active">Aula</li>
+        </ol>
+    </div>
 
-    <x-alert />
+    <div class="card mb-4">
+        <div class="card-header hstack gap-2">
+            <span>Cadastrar</span>
+            <span class="ms-auto d-sm-flex flex-row">
+                <a href="{{ route('classe.index', ['course' => $course->id]) }}" class="btn btn-info btn-sm me-1 mb-1 mb-sm-0">Listar</a>
+            </span>
+        </div>
+        <div class="card-body">
+            <x-alert />
 
-    <form action="{{ route('classe.store')}}" method="POST">
-        @csrf
-        @method('POST')
+            <form class="row g-3" action="{{ route('classe.store')}}" method="POST">
+                @csrf
+                @method('POST')
+                <input type="hidden" name="course_id" id="course_id" value="{{ $course_id }}">
+                <div class="col-12">
+                    <label for="course" class="form-label">Curso</label>
+                    <input type="text" name = "course" class="form-control" id="course"  value="{{ $course->name }}" disabled >
+                </div>
+                <div class="col-12">
+                    <label for="name" class="form-label">Aula</label>
+                    <input type="text" name = "name" class="form-control" id="name" placeholder="Nome da aula" value="{{ old('name')}} ">
+                </div>
+                <div class="col-12">
+                    <label for="description" class="form-label">Descrição</label>
+                    <textarea name="description" id="description" cols="30" rows="10" class="form-control" placeholder="Descrição da aula" >{{ old('description') }}</textarea>
+                </div>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-success btn-sm">Cadastrar</button>
+                  </div>
+            </form>
+            
+        </div>
+    </div>
 
-        <input type="hidden" name="course_id" id="course_id" value="{{ $course_id }}">
 
-        <label>Curso: </label>
-        <input type="text" name="name_course" id="name_course" value="{{ $course->name }}" disabled ><br><br>
-        
-        <label>Nome: </label>
-        <input type="text" name="name" id="name" placeholder="Nome da aula" value="{{old('name')}}" required><br><br>
-
-        <label>Descrição: </label>
-        <input type="text" name="description" id="description" placeholder="Descrição da Aula" value="{{old('description')}}" required><br><br>
-
-        <button type="submit">Cadastrar</button>
-
-
-    </form>
+</div>
 
 @endsection
--- resources/views/classes/edit.blade.php
+++ resources/views/classes/edit.blade.php
@@ -2,28 +2,56 @@
 
 @section('content')
 
-    <h2>Editar Aula</h2>
+<div class="container-fluid px-4">
+    <div class="mb-1 hstack gap-2">
+        <h2 class="mt-3">Aula</h2>
 
-    <a href="{{ route('classe.index', ['course' => $classe->course_id]) }}" class="link-button">Aulas</a><br>
+        <ol class="breadcrumb mb-3 mt-3 ms-auto">
+            <li class="breadcrumb-item">
+                <a href="#" class="text-decoration-none">Dashboard</a>
+            </li>
+            <li class="breadcrumb-item">
+                <a href="{{ route('classe.index', ['course' => $classe->course_id]) }}" class="text-decoration-none">Aulas</a>
+            </li>
+            <li class="breadcrumb-item active">Curso</li>
+        </ol>
+    </div>
 
-    <x-alert />
+    <div class="card mb-4">
+        <div class="card-header hstack gap-2">
+            <span>Editar</span>
+            <span class="ms-auto d-sm-flex flex-row">
+                <a href="{{ route('classe.index', ['course' => $classe->course_id]) }}" class="btn btn-info btn-sm me-1 mb-1 mb-sm-0">Listar</a>
+                <a href="{{ route('classe.show', ['classe' => $classe->id]) }}" class="btn btn-primary btn-sm me-1 mb-1 mb-sm-0">Visualizar</a>
+            </span>
+        </div>
+        <div class="card-body">
+            <x-alert />
 
-    <form action="{{ route('classe.update', ['classe' => $classe->id]) }}" method="POST">
-        @csrf
-        @method('PUT')
+            <form class="row g-3" action="{{ route('classe.update', ['classe' => $classe->id]) }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class="col-12">
+                    <label for="course" class="form-label">Curso</label>
+                    <input type="text" name = "course" class="form-control" id="course"  value="{{ $classe->course->name }}" disabled >
+                </div>
+                <div class="col-12">
+                    <label for="name" class="form-label">Aula</label>
+                    <input type="text" name = "name" class="form-control" id="name" placeholder="Nome da aula" value="{{old('name', $classe->name)}}">
+                </div>
+                <div class="col-12">
+                    <label for="description" class="form-label">Descrição</label>
+                    <textarea name="description" id="description" cols="30" rows="10" class="form-control" placeholder="Descrição da aula" >{{old('name', $classe->description)}}</textarea>
+                </div>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-warning btn-sm">Editar</button>
+                  </div>
+            </form>
+            
+        </div>
+    </div>
 
-        <label>Curso: </label>
-        <input type="text" name="name_course" id="name_course" value="{{ $classe->course->name }}" disabled ><br><br>
-        
-        <label>Nome: </label>
-        <input type="text" name="name" id="name" placeholder="Nome da aula" value="{{old('name', $classe->name)}}" required><br><br>
 
-        <label>Descrição: </label>
-        <input type="text" name="description" id="description" placeholder="Descrição da Aula" value="{{old('description', $classe->description)}}" required><br><br>
-
-        <button type="submit">Editar</button>
-
-
-    </form>
+</div>
 
 @endsection
